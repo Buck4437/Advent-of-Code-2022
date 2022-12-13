d118912b314bdc866c0fd7aa3f03a2719dfebec2
Buck4437 <51261544+Buck4437@users.noreply.github.com>
2022-12-13 14:10:07 +0800

    Day 13

--- Day13/main.py
+++ Day13/main.py
@@ -2,3 +2,35 @@ from aoc import *
 
 with open("input.txt") as f:
     s = "\n".join([line.strip() for line in f])
+
+
+def compare(left, right):
+    if isinstance(left, int) and isinstance(right, int):
+        return 1 if left < right else 0 if left == right else -1
+
+    lst_l = [left] if isinstance(left, int) else left
+    lst_r = [right] if isinstance(right, int) else right
+
+    for i in range(min(len(lst_l), len(lst_r))):
+        sub_l, sub_r = lst_l[i], lst_r[i]
+        result = compare(sub_l, sub_r)
+        if result != 0:
+            return result
+    return compare(len(lst_l), len(lst_r))
+
+
+pairs = s.split("\n\n")
+tot = 0
+for i, pair in enumerate(pairs):
+    a, b = pair.split("\n")
+    ap, bp = eval(a), eval(b)
+    if compare(ap, bp) >= 0:
+        tot += (i + 1)
+print(tot)
+
+
+packets = [eval(x) for x in s.split("\n") if x != ""]
+a, b = [[2]], [[6]]
+packets.extend([a, b])
+sorted_packets = merge_sort(packets, compare)
+print(mul([sorted_packets.index(x) + 1 for x in [a, b]]))
--- aoc.py
+++ aoc.py
@@ -19,3 +19,26 @@ def mul(lst):
     for x in lst:
         result *= x
     return result
+
+
+def merge_sort(lst, comparator=None):
+    L = lst[:]
+    if comparator is None:
+        def comparator(x, y):
+            return 1 if x < y else 0 if x == y else -1
+    if len(lst) <= 1:
+        return L
+    half = len(lst) // 2
+    return __merge(merge_sort(L[:half], comparator), merge_sort(L[half:], comparator), comparator)
+
+
+def __merge(l1, l2, comparator):
+    lst = []
+    while min(len(l1), len(l2)) > 0:
+        a, b = l1[0], l2[0]
+        if comparator(a, b) > -1:
+            lst.append(l1.pop(0))
+        else:
+            lst.append(l2.pop(0))
+    return lst + l1 + l2
+
